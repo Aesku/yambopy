f4f876cf2b802c1f4812de75b7f205856cd08ff2
Ali Esquembre Kučukalić <aliek@Alis-MacBook-Air.local>
2025-01-28 16:33:17 +0100

    magnons tutorial

--- tutorial/databases_yambopy/magnondb/magn_on_bands_interpolated.py
+++ tutorial/databases_yambopy/magnondb/magn_on_bands_interpolated.py
@@ -31,10 +31,10 @@ if __name__ == "__main__":
     ax  = fig.add_axes( [ 0.15, 0.15, 0.80, 0.80 ])
 
     # In case of problems with the interpolation, try to increase lpratio
-    exc_on_bands = ymagn.interpolate(yel,path,states,lpratio=10,f=None,verbose=True)
+    magn_on_bands = ymagn.interpolate(yel,path,states,lpratio=10,f=None,verbose=True)
 
     # The 'size' argument controls the weight widths
-    exc_on_bands.plot_ax(ax,c_bands='grey',c_weights='red',size=1.,alpha_weights=0.5)
+    magn_on_bands.plot_ax(ax,c_bands='k',c_weights='red',size=1.,alpha_weights=0.5)
 
     ax.set_ylim(-7.5,12.)
     plt.show()
